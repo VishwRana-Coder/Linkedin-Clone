52e4df0a9c96cb37c6cf66e85a5b449413c9ff88
Vishw Rana <vishw.rana.k@gmail.com>
2025-10-08 10:31:31 +0530

    React Router Added

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Home from "./pages/home/index";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./index.scss"
+
+const Home = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Home
